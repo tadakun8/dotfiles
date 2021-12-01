27921aa52c166272783d013b6d6e81ff33e809cd
tadakun8 <tadakun8.lab@gmail.com>
2021-12-01 15:51:45 +0900

    refactor: Suppress warning of shellcheck

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
-#!/bin/sh
-
-# Show the name of the directory where this script is located
-# SCRIPT_DIR=$(cd $(dirname $0); pwd)
-# # Create a symbolic link to dotfile in my home directory
-# ls -AF | grep -v '/' | grep '^\.' | xargs -I {} ln -sf ${SCRIPT_DIR}/{} $HOME/{}
+#!/bin/bash
 
 # Create a symbolic link to dotfile in my home directory
 # $1: dotfile path
 function create_symbolic_link_to_homedir() {
   file_name=$(basename "${1}")
-  ln -sf "${1}" $HOME/"${file_name}"
+  ln -sf "${1}" "${HOME}"/"${file_name}"
 }
 export -f create_symbolic_link_to_homedir
 
-cd $(dirname $0)
+cd "$(dirname "${0}")" || (echo 'Failure'; exit)
 
-find $(pwd) -type f -mindepth 2 -maxdepth 2 | grep -v .git | xargs -I{} bash -c "create_symbolic_link_to_homedir {}"
+find "$(pwd)" -type f -mindepth 2 -maxdepth 2 | grep -v .git | xargs -I{} bash -c "create_symbolic_link_to_homedir {}"
